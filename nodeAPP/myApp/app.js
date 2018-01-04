d49cd2103ff68c501209407a44bc2c725b38e973
youTT <cdyoutingting1@jd.com>
2018-01-04 17:50:11 +0800

    自己的express框架

---
+++ nodeAPP/myApp/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//设置可以访问public里面的静态资源,访问地址直接为/images or /javascripts等，忽略public
+app.use(express.static(path.join(__dirname, 'public')));
+//设置fileTest文件夹也为静态资源文件夹，访问的时候回根据配置文件的顺序进行搜索访问
+app.use(express.static(path.join(__dirname, 'fileTest')));
+app.use(express.static('files'));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
